e00093232f247bd426825a51ae52b81bf91d3a1b
anhtuan1811999 <anhtuannguyen226@gmail.com>
2023-02-28 01:36:39 -0800

    first

---
+++ app/src/main/java/com/example/snakegame/MainActivity.java
@@ -0,0 +1,90 @@
+package com.example.snakegame;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatImageButton;
+
+import android.os.Bundle;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements SurfaceHolder.Callback {
+
+    private SurfaceView surfaceView;
+    private TextView scoreTV;
+
+    //snake moving position. Values must be right, left, top, bottom
+    //default snake move to right
+    private String movingPosition = "right";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //getting surfaceview and score textview from xml file
+        surfaceView = findViewById(R.id.surfaceView);
+        scoreTV = findViewById(R.id.scoreTV);
+
+        //getting ImageButtons from xml file
+        final AppCompatImageButton topBtn = findViewById(R.id.topBtn);
+        final AppCompatImageButton leftBtn = findViewById(R.id.leftBtn);
+        final AppCompatImageButton rightBtn = findViewById(R.id.rightBtn);
+        final AppCompatImageButton bottomBtn = findViewById(R.id.bottomBtn);
+
+        // adding callback to surfaceview
+        surfaceView.getHolder().addCallback(this);
+
+        topBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //check if previous moving is not bottom. Snake can't move,
+                //For example if snake moving to bottom then snake can't directly start moving to top
+                //snake must take right of left first then top
+                if (movingPosition.equals("bottom")){
+                    movingPosition="top";
+                }
+            }
+        });
+        leftBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (movingPosition.equals("right")){
+                    movingPosition="left";
+                }
+            }
+        });
+        rightBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (movingPosition.equals("left")){
+                    movingPosition="right";
+                }
+            }
+        });
+        bottomBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (movingPosition.equals("top")){
+                    movingPosition="bottom";
+                }
+            }
+        });
+    }
+
+    @Override
+    public void surfaceCreated(@NonNull SurfaceHolder surfaceHolder) {
+
+    }
+
+    @Override
+    public void surfaceChanged(@NonNull SurfaceHolder surfaceHolder, int i, int i1, int i2) {
+
+    }
+
+    @Override
+    public void surfaceDestroyed(@NonNull SurfaceHolder surfaceHolder) {
+
+    }
+}
